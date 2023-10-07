667a58d6d99d1384304ca85996ca48da21e84e1f
davidthus <dvdnicorici05@gmail.com>
2023-10-07 13:13:41 +0100

    delete hooks.server.ts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import FirebaseApp from '$lib/components/FirebaseApp.svelte';
+	import { auth, db as firestore, storage } from '$lib/firebase';
+</script>
+
+<FirebaseApp {auth} {firestore} {storage}>
+	<slot />
+</FirebaseApp>
