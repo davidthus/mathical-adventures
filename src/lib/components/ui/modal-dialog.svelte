89c30d74330be90f094859fc3630db8a9ce3f95b
davidthus <dvdnicorici05@gmail.com>
2023-10-08 19:22:26 +0100

    add: custom elements

---
+++ src/lib/components/ui/modal-dialog.svelte
@@ -0,0 +1,20 @@
+<svelte:options customElement="modal-dialog" />
+
+<script lang="ts">
+	import { modal } from '$lib/stores/modal';
+	export let name = 'default';
+	export let esc = false;
+
+	function closeModal() {
+		modal.set(null);
+	}
+</script>
+
+<div class="backdrop" class:show={$modal === name} on:click={closeModal}>
+	<div on:click|stopPropagation class="inner" class:inner-show={$modal === name}>
+		{#if esc}
+			<kbd class="esc" on:click={closeModal}>esc</kbd>
+		{/if}
+		<slot />
+	</div>
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelte({ compilerOptions: { customElement: true } })],
+
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
