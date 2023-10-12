f90f563958ff488700fdc86a9792553f4c37ba38
davidthus <dvdnicorici05@gmail.com>
2023-10-12 23:31:45 +0100

    create landing page markup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<section>
+	<h1>Where maths meets storytelling</h1>
+	<h2>A relaxing activity for primary school children</h2>
+	<a href="/acts">Buy ACTS</a>
+</section>
+<main>
+	<section>
+		<h3>Getting the characters through peril</h3>
+		<p>
+			There are several characters that need YOUR help to get through their battles. Will you help?
+		</p>
+		<!-- <img src="" alt=""> -->
+	</section>
+	<section>
+		<h3>Maths exercises integrated into the story</h3>
+		<p>
+			Every chapter you will find a multitude of simple maths exercises built to give understanding
+			of the topic
+		</p>
+	</section>
+	<section>
+		<h3>Every Act is based on different topics</h3>
+		<p>Need help with fractions or multiplication? Find the Act to help you!</p>
+	</section>
+</main>
