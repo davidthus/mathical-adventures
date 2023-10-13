c112f5c500f9649831b588fec39fdf4e9fb6b586
davidthus <dvdnicorici05@gmail.com>
2023-10-13 23:57:29 +0100

    create pro and acts pages

---
+++ src/routes/acts/+page.svelte
@@ -0,0 +1,11 @@
+<section>
+	<h1>ACTS</h1>
+	<p>Here is your selection of acts to choose from:</p>
+</section>
+
+<section>
+	<h2>What topics do you need help with?</h2>
+	<ul>
+		<!-- loop through acts -->
+	</ul>
+</section>
---
+++ src/routes/pro/+page.svelte
@@ -0,0 +1,8 @@
+<section>
+	<h1>Become a worldwide hero!</h1>
+	<p>Purchase a pro subscription to gain access to all the acts!</p>
+</section>
+
+<section>
+	<!-- different pro options -->
+</section>
